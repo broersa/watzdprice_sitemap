d7ff30a1cefe216b69c51c6b6124f324382ccef4
Andre Broers <broersa@Andres-MacBook-Air.local>
2017-03-14 21:22:25 +0100

    rework

--- src/app/main.js
+++ src/app/main.js
@@ -38,7 +38,6 @@ module.exports = {
       sitemap = '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
       sitemap = sitemap + '<url><loc>https://www.watzdprice.nl/</loc></url>';
       sitemap = sitemap + '<url><loc>https://www.watzdprice.nl/about</loc></url>';
-      sitemap = sitemap + '</urlset>';
       endSitemap(sitemap, path, 0);
       var c = 0;
       var smc = 1;
